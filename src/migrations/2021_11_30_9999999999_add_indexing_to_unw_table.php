c9e0f219872b9e9503ecf297788393d5991adc13
Ravi Gulia <ravigulia@bitbucket.org>
2021-11-11 13:20:47 +0530

    unw

--- src/migrations/2021_11_30_9999999999_add_indexing_to_unw_table.php
+++ src/migrations/2021_11_30_9999999999_add_indexing_to_unw_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class addIndexToUnwAnsTable extends Migration
+class addIndexingToUnwTable extends Migration
 {
     /**
      * Run the migrations.
